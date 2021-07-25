28c93886fb13445df13c554313905bffcedacba6
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-07-25 20:06:23 +0530

    Header & Footer Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import react from 'react'
+import react from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
-const App = () =>  {
+const App = () => {
   return (
     <>
-     <h1>Welcome To ProShop</h1> 
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
