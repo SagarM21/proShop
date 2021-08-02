0a781c9487f3e7e94120d17f22191fa816d5abfd
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-08-02 22:20:41 +0530

    Bringing Redux state into HomeScreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,7 +9,7 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
-      return { loading: true, products: action.payload }
+      return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload }
     default:
