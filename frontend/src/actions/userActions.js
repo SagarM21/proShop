9f3a547debd01b1d044490de94ba4bc597d6f9c0
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-08-08 15:57:55 +0530

    Update User Profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,12 +8,17 @@ import {
   USER_LOGIN_FAIL,
   USER_REGISTER_FAIL,
   USER_LOGOUT,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants"
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: "USER_LOGIN_REQUEST",
+      type: USER_LOGIN_REQUEST,
     })
 
     const config = {
@@ -29,7 +34,7 @@ export const login = (email, password) => async (dispatch) => {
     )
 
     dispatch({
-      type: "USER_LOGIN_SUCCESS",
+      type: USER_LOGIN_SUCCESS,
       payload: data,
     })
 
@@ -53,7 +58,7 @@ export const logout = () => (dispatch) => {
 export const register = (name, email, password) => async (dispatch) => {
   try {
     dispatch({
-      type: "USER_REGISTER_REQUEST",
+      type: USER_REGISTER_REQUEST,
     })
 
     const config = {
@@ -69,12 +74,12 @@ export const register = (name, email, password) => async (dispatch) => {
     )
 
     dispatch({
-      type: "USER_REGISTER_SUCCESS",
+      type: USER_REGISTER_SUCCESS,
       payload: data,
     })
 
     dispatch({
-      type: "USER_LOGIN_SUCCESS",
+      type: USER_LOGIN_SUCCESS,
       payload: data,
     })
 
@@ -93,7 +98,7 @@ export const register = (name, email, password) => async (dispatch) => {
 export const getUserDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({
-      type: "USER_DETAILS_REQUEST",
+      type: USER_DETAILS_REQUEST,
     })
 
     const {
@@ -110,12 +115,46 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/users/${id}`, config)
 
     dispatch({
-      type: "USER_DETAILS_SUCCESS",
+      type: USER_DETAILS_SUCCESS,
       payload: data,
     })
   } catch (error) {
     dispatch({
       type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    })
+  }
+}
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/profile`, user, config)
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import { Row, Col, Form, Button } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { getUserDetails } from "../actions/userActions"
+import { getUserDetails, updateUserProfile } from "../actions/userActions"
 
 import React from "react"
 
@@ -22,6 +22,9 @@ const ProfileScreen = ({ location, history }) => {
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
 
   useEffect(() => {
     if (!userInfo) {
@@ -41,7 +44,7 @@ const ProfileScreen = ({ location, history }) => {
     if (password !== confirmPassword) {
       setMessage("Passwords do not match")
     } else {
-      //DISPATCH UPDATE PROFILE
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
   }
 
@@ -51,6 +54,7 @@ const ProfileScreen = ({ location, history }) => {
         <h2>User Profile</h2>
         {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
+        {success && <Message variant='success'>Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId='name'>
