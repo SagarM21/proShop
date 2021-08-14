d6165cd91bd6df10fb80bd45152e2e3fd9bd23cb
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-08-14 15:37:53 +0530

    Admin Middleware & Users Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -9,8 +9,8 @@ import {
 import { protect } from "../middleware/authMiddleware.js"
 
 router.route("/").post(protect, addOrderItems)
-router.route("/:id").get(protect, getOrderById)
 router.route("/:id/pay").put(protect, updateOrderToPaid)
 router.route("/myorders").get(protect, getMyOrders)
+router.route("/:id").get(protect, getOrderById)
 
 export default router
