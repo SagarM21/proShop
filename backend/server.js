74a8d2fec7969779ae934a718c5d572acc63b099
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-08-18 14:52:58 +0530

    Morgan & Create review endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import colors from "colors"
+import morgan from "morgan"
 
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
@@ -15,6 +16,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
 
 app.use(express.json())
 
